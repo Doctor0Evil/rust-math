96d33d66f1e65c3c1214bf2fe29f50f4a8c6bcb8
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-02-08 16:44:01 -0700

    Update parse.rs

--- src/math/parse.rs
+++ src/math/parse.rs
@@ -1,7 +1,10 @@
-use crate::error::AppError;
 use symbolica::atom::Atom;
 use symbolica::parser::parse;
+use crate::error::AppError;
+use super::backend::{SymbolicBackend, symbolica_backend::SymbolicaBackend};
 
-pub fn parse_expression(expr: &str) -> Result<Atom, AppError> {
-    parse(expr).map_err(|e| AppError::BadRequest(format!("Parse error: {}", e)))
+pub type Expr = <SymbolicaBackend as SymbolicBackend>::Expr;
+
+pub fn parse_expression(expr: &str) -> Result<Expr, AppError> {
+    SymbolicaBackend::parse(expr)
 }
