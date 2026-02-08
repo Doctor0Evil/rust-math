792155a9b2bee25e72e24fc2a86a9d67f331ebfc
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-02-08 16:46:22 -0700

    Update simplify.rs

--- src/math/simplify.rs
+++ src/math/simplify.rs
@@ -7,7 +7,7 @@ use super::parse::Expr;
 pub fn simplify_expression(expr: &Expr) -> Result<Expr, AppError> {
     SymbolicaBackend::simplify(expr)
 }
-
+                                                     }
     let simplified = expr.simplify();
     Ok(simplified)
 }
