4372e31f1b1f5dd77f884eb69559b5d9c1e355ff
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-02-08 16:27:11 -0700

    Create mod.rs

---
+++ src/math/mod.rs
@@ -0,0 +1,13 @@
+pub mod parse;
+pub mod simplify;
+pub mod differentiate;
+pub mod integrate;
+pub mod evaluate;
+pub mod matrix;
+pub mod stats;
+
+pub use parse::parse_expression;
+pub use simplify::simplify_expression;
+pub use differentiate::differentiate_expression;
+pub use evaluate::evaluate_expression;
+pub use matrix::solve_linear_system;
